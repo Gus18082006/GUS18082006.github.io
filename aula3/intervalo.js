2e4c607318b9711fc6cb9a067d3eb7064dfbcd9a
GUS18082006 <gustavoindaia12345@gmail.com>
2025-10-04 12:22:22 -0300

    aula 3

---
+++ aula3/intervalo.js
@@ -0,0 +1,19 @@
+
+        setTimeout(function () {
+          document.getElementById("tempo").textContent = "Bem-vindo";
+        } , 2000);
+
+        var contador = 0
+
+        setInterval (function () {
+          contador = contador + 1
+
+          document.getElementById("contador-elemento").textContent = contador;
+
+        } ,1000);
+         
+    
+    
+    
+
+    
